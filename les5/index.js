89bee6fd95d4fca38ae3f89958cf6497f4fa894c
Шаврова Марина Петровна <maryna.shavrova@privatbank.ua>
2022-09-18 14:30:32 +0300

    Added homework 5, lesson 6

---
+++ les5/index.js
@@ -0,0 +1,29 @@
+let menu = {
+  width: 200,
+  height: 300,
+  title: "My menu",
+};
+
+function multiplication(obj) {
+  for (let key in obj) {
+    if (typeof obj[key] == "number") {
+      obj[key] *= 2;
+    }
+  }
+  console.log(obj);
+}
+
+function deleteValue(obj) {
+  delete obj.height;
+  delete obj.width;
+  console.log(obj);
+}
+
+function replaceValue(obj, key, value) {
+    obj[key] = value;
+    console.log(obj);
+}
+
+multiplication(menu)
+deleteValue(menu);
+replaceValue(menu, "title", "Clear");
